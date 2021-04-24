e80e095ca2a2d9de47c1944bce744de8b5d9103b
Wilbert Navarrete <wilmion92@gmail.com>
2021-04-24 04:06:04 +0200

    finish configuration

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import FoxbelMusicIcon from './static/icons/foxbel-music-icon.png';
+
+const container = document.getElementById('app');
+
+ReactDOM.render( <img src={FoxbelMusicIcon} alt="test"/> , container )
---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: path.resolve(__dirname , 'src/index.js'),
+    output: {
+        filename: 'js/[fullhash].js',
+        path: path.resolve(__dirname , 'dist/'),
+        publicPath: '/'
+    },
+    resolve: { extensions: ['.js' , '.jsx'] },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.sass$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.png$/,
+                use: {
+                    loader: 'file-loader',
+                    options: {
+                        name: '[hash].[ext]',
+                        outputPath: 'static/images/'
+                    }
+                }
+            }
+        ]
+    },
+    plugins : [
+        new MiniCssExtractPlugin({
+            filename: 'css/[fullhash].css',
+            linkType: 'text/css'
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: path.resolve(__dirname , 'public/index.html')
+        })
+    ],
+    devServer: {
+        port: 8080,
+        open:true,
+        historyApiFallback:true
+    }
+}
