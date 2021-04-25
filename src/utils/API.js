f96756c320abf844686cce64118f79111fc312cf
Wilbert Navarrete <wilmion92@gmail.com>
2021-04-25 21:01:26 +0200

    fix

--- src/utils/API.js
+++ src/utils/API.js
@@ -4,7 +4,7 @@ export const GET = async (url) => {
     let error = null;
     let data = null;
     try {
-        const dataApi = await axios.get(url);
+        const dataApi = await axios.get(`https://cors-anywhere.herokuapp.com/${url}`);
         data = dataApi.data;
     }catch (e) {
         error = 'Hubo un error al pedir los datos , por favor recarge la pagina'
--- webpack.config.js
+++ webpack.config.js
@@ -56,7 +56,8 @@ module.exports = {
         }),
         new HtmlWebpackPlugin({
             filename: 'index.html',
-            template: path.resolve(__dirname , 'public/index.html')
+            template: path.resolve(__dirname , 'public/index.html'),
+            favicon: "./src/static/icons/foxbel-music-icon@2x.png"
         })
     ],
     devServer: {
